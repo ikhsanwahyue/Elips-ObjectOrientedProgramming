22a0aecdccea4ba26aec33556d925c82986f00d5
iwahyuendriarto@gmail.com <ikhsanwahyue_iwahyuendriarto@gmail.com>
2026-04-25 12:34:16 +0700

    Ikhsan: Sinkronisasi isi class Elips dengan logika final

--- src/pElips/model/Elips.java
+++ src/pElips/model/Elips.java
@@ -8,6 +8,59 @@ package pElips.model;
  *
  * @author LENOVO
  */
-public class Elips {
+public class Elips extends BendaGeometri implements KalkulasiGeometri {
     
+    // Protected agar bisa diakses langsung oleh class benda 3D berbasis elips (Bola, Kerucut, dll..)
+    protected double sumbuA;
+    protected double sumbuB;
+    
+    public Elips(String nama, double a, double b) {
+        super(nama);
+        this.sumbuA = a;
+        this.sumbuB = b;
+    }
+    
+    // Implementasi Pilar Encapsulation
+    public double getSumbuA() {
+        return sumbuA;
+    }
+    
+    public void setSumbuA(double sumbuA) {
+        this.sumbuA = sumbuA;
+    }
+    
+    public double sumbuB() {
+        return sumbuB;
+    }
+    
+    public void setSumbuB(double sumbuB) {
+        this.sumbuB = sumbuB;
+    }
+    
+    // Implementasi Pilar Override & Kalkulasi
+    @Override
+    public double hitungLuas() {
+        return Math.PI * sumbuA * sumbuB;
+    }
+    
+    @Override
+    public double hitungKeliling() {
+        return Math.PI * (3 * (sumbuA + sumbuB) - Math.sqrt((3 * sumbuA + sumbuB) * (sumbuA + 3 * sumbuB)));
+    }
+    
+    @Override
+    public double hitungVolume() {
+        
+        // Elips adalah benda 2D, jadi volumenya nol
+        return 0.0;
+    }
+    
+    @Override
+    public void cetakInfo() {
+        System.out.println("--- Data Geometri: " + getNamaBenda() + " ---");
+        System.out.println("Sumbu A (Mayor) : " + sumbuA);
+        System.out.println("Sumbu B (Minor) : " + sumbuB);
+        System.out.println("Luas            : " + String.format("%.2f", hitungLuas()));
+        System.out.println("Keliling        : " + String.format("%.2f", hitungKeliling()));
+    }
 }
