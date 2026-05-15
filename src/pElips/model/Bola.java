c3135e4db5f89419c61d9f806e23bf557efd41bb
laharputraaa <laharnuansa2@gmail.com>
2026-05-15 17:31:24 +0700

    menambah codingan bola dan tabung

--- src/pElips/model/Bola.java
+++ src/pElips/model/Bola.java
@@ -8,6 +8,36 @@ package pElips.model;
  *
  * @author LENOVO
  */
-public class Bola {
-    
+public class Bola extends BendaGeometri implements KalkulasiGeometri {
+    protected double jariJari;
+
+    public Bola() {
+        // setNamaBenda("Bola");
+        super("Bola");
+    }
+
+    public void setJariJari(double r) {
+        this.jariJari = r;
+    }
+
+    @Override
+    public double hitungLuas() {
+        return 4 * Math.PI * Math.pow(jariJari, 2);
+    }
+
+    @Override
+    public double hitungKeliling() {
+        return 0;
+    }
+
+    @Override
+    public double hitungVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(jariJari, 3);
+    }
+
+    @Override
+    public void cetakInfo() {
+        System.out.println("Nama: " + getNamaBenda());
+        System.out.println("Volume: " + hitungVolume());
+    }
 }
--- src/pElips/model/Tabung.java
+++ src/pElips/model/Tabung.java
@@ -8,6 +8,48 @@ package pElips.model;
  *
  * @author LENOVO
  */
-public class Tabung {
-    
+
+public class Tabung extends BendaGeometri implements KalkulasiGeometri {
+    private double jariJari;
+    private double tinggi;
+
+    public Tabung() {
+        super("Tabung");
+        setNamaBenda("Tabung");
+    }
+
+    public void setJariJari(double r) {
+        this.jariJari = r;
+    }
+
+    public void setTinggi(double t) {
+        this.tinggi = t;
+    }
+
+    @Override
+    public double hitungLuas() {
+        // Luas Permukaan: 2 * pi * r * (r + t)
+        return 2 * Math.PI * jariJari * (jariJari + tinggi);
+    }
+
+    @Override
+    public double hitungKeliling() {
+        // Keliling alas: 2 * pi * r
+        return 2 * Math.PI * jariJari;
+    }
+
+    @Override
+    public double hitungVolume() {
+        // Volume: pi * r^2 * t
+        return Math.PI * Math.pow(jariJari, 2) * tinggi;
+    }
+
+    @Override
+    public void cetakInfo() {
+        System.out.println("Nama Benda : " + getNamaBenda());
+        System.out.println("Jari-jari  : " + jariJari);
+        System.out.println("Tinggi     : " + tinggi);
+        System.out.println("Volume     : " + hitungVolume());
+        System.out.println("Luas Perm. : " + hitungLuas());
+    }
 }
